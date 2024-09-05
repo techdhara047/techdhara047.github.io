821cd75d4748d0a8b3d67e363c84f7b93f0c3f2c
techdhara047 <techdhara047@gmail.com>
2024-09-06 04:12:52 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://techdhara047.github.io",
+  integrations: [sitemap()]
 });
